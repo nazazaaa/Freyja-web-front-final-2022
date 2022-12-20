0bfaead632baf8c69cf6f3da6c8c1ea29d5da4f5
altyngul02 <121065697+altyngul02@users.noreply.github.com>
2022-12-20 17:58:32 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function ozgeru(obj) {
+    document.getElementById('photo1').src = 'eee.jpg';}
+function ozgeru2(obj) {
+    document.getElementById('dish').src = 'dish2.webp';}
+function ozgeru3(obj) {
+    document.getElementById('glass').src = 'glass2.webp';};
+function ozgeru4(obj) {
+    document.getElementById('jug').src = 'jug2.webp';}
+function ozgeru5(obj) {
+    document.getElementById('vase').src = 'vase2.webp';}
+function ozgeru6(obj) {
+    document.getElementById('cup').src = 'cup2.webp';}
+function ozgeru7(obj) {
+    document.getElementById('cauldron').src = 'caul2.webp';}
+function ozgeru8(obj) {
+    document.getElementById('bowl').src = 'bowl2.webp';}
+function ozgeru9(obj) {
+    document.getElementById('breadbasket').src = 'bread2.webp';}
+    
+
+function out(){
+    document.getElementById('photo1').src = 'шоп.png';
+    document.getElementById('dish').src = 'dishh.webp';
+    document.getElementById('glass').src = 'glass.webp';
+    document.getElementById('vase').src = 'vase.webp';
+    document.getElementById('jug').src = 'jug.webp';
+    document.getElementById('cup').src = 'cup.webp';
+    document.getElementById('cauldron').src = 'caul.webp';
+    document.getElementById('bowl').src = 'bowl.webp';
+    document.getElementById('breadbasket').src = 'bread.webp';
+}
